d867c62fc576fc523b5ca531b3f41fb8cbac5aac
HailonZhon <122834546+HailonZhon@users.noreply.github.com>
2024-02-08 15:09:06 +0800

    rebuild all code

--- shensibackend/app/api/api_v1/payment/alipayment.py
+++ shensibackend/app/api/api_v1/payment/alipayment.py
@@ -1,17 +1,6 @@
-from datetime import datetime
-import os
-import random
-from dotenv import load_dotenv
-import logging
-
 from fastapi import APIRouter, Depends
-from alipay.aop.api.DefaultAlipayClient import DefaultAlipayClient
-from alipay.aop.api.domain.AlipayTradePagePayModel import AlipayTradePagePayModel
-from alipay.aop.api.request.AlipayTradePagePayRequest import AlipayTradePagePayRequest
-from alipay.aop.api.AlipayClientConfig import AlipayClientConfig
 from app.dependencies import get_current_user
 from fastapi import APIRouter, Request, Depends
-from alipay.aop.api.response.AlipayTradePagePayResponse import AlipayTradePagePayResponse
 from app.models.shensimodels import UserModel
 from app.dependencies import get_current_user
 from app.services.payment_service import initiate_payment, process_payment_notification
