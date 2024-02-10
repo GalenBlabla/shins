b3ca0405519d741f4448ac68d583cdb4bcfabb7a
HailonZhon <122834546+HailonZhon@users.noreply.github.com>
2024-02-10 23:13:48 +0800

    rebuild alipay pro

--- shensibackend/app/services/user_services/payment_service.py
+++ shensibackend/app/services/user_services/payment_service.py
@@ -24,6 +24,7 @@ def load_private_key_from_file(private_key_path):
     try:
         with open(private_key_path, 'r') as file:
             private_key = file.read()
+        print("private_key:",private_key)
         return private_key
     except Exception as e:
         print(f"Error loading private key from {private_key_path}: {e}")
@@ -40,6 +41,7 @@ def load_public_key_from_file(public_key_path):
     try:
         with open(public_key_path, 'r') as file:
             public_key = file.read()
+        print("public_key:",public_key)
         return public_key
     except Exception as e:
         print(f"Error loading public key from {public_key_path}: {e}")
